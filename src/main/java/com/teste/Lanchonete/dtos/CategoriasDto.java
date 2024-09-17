41832ede669424a4bbbe0f3911c285166e1314b2
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2024-09-17 19:15:49 -0300

    Criação cadastro de categorias

--- src/main/java/com/teste/Lanchonete/dtos/CategoriasDto.java
+++ src/main/java/com/teste/Lanchonete/dtos/CategoriasDto.java
@@ -2,13 +2,16 @@ package com.teste.Lanchonete.dtos;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
-public class CategoriaDto {
+public class CategoriasDto {
     Integer idCategoria;
+
+    @NonNull
     String nomeCategoria;
 
 }
--- src/main/java/com/teste/Lanchonete/entities/Categorias.java
+++ src/main/java/com/teste/Lanchonete/entities/Categorias.java
@@ -1,10 +1,7 @@
 package com.teste.Lanchonete.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,7 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "tb_categoria")
-public class Categoria {
+public class Categorias {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,7 +23,7 @@ public class Categoria {
      * RELACIONAMENTOS
      *********************/
 
-    @OneToMany(mappedBy = "categoria")
+    @OneToMany(mappedBy = "categorias")
     private List<Produtos> produtosList;
 
     @ManyToOne
